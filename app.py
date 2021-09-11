85cbb26b969819f6900e0becad8bce049ec6965f
HaithamTalouli <63872414+HaithamTalouli@users.noreply.github.com>
2021-09-11 12:53:59 -0400

    credit choices added
    Implemented credit choices in Search file

--- app.py
+++ app.py
@@ -11,8 +11,16 @@ app.config['UPLOAD_FOLDER'] = picFolder
 def home():
     return render_template("form.html")
 
-@app.route("/templates/search.html")
+@app.route("/templates/search.html", methods = ["GET", "POST"])
 def search():
+    if request.method == "POST":
+        common_benefits = request.form.get("benefit")
+        occupation = request.form.get("occupation")
+        interest =  request.form.get("interest")
+        if(common_benefits == "Hotel and Travel Points"):
+            return "This is a hotel boss"
+
+        return "Your benefits are " + common_benefits + "\n" + "Your occupation is " + occupation + "\n" + "Your occupation is " + interest
     return render_template("search.html")
 
 @app.route("/templates/profile.html")
