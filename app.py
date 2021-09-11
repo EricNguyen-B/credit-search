3d043df7aefcd557ad0f7aa2d401d74a04790b66
Jays-Nemesis <83093869+Jays-Nemesis@users.noreply.github.com>
2021-09-11 12:54:18 -0400

    Credit images
    Just tested with images

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, request, render_template
 import os
 
@@ -19,6 +20,13 @@ def search():
 def profile():
     return render_template("profile.html")
 
+@app.route("/templates/cards_images.html")
+def cards_images():
+    #first_name = request.form.get("fname")
+    #last_name = request.form.get("lname")
+    #credit_score = request.form.get("creditsore")
+    photo_file = os.path.join(app.config['UPLOAD_FOLDER'], 'chase_sapphire_preferred.jpg','discover_it_secured','citi_double_cash_card')
+    return render_template("cards_images.html", user_image = photo_file)
 
 if __name__ == "__main__":
     app.run() 
