770e790651cb126ae554a72d80e9bf11840ecd58
HaithamTalouli <63872414+HaithamTalouli@users.noreply.github.com>
2021-09-11 22:34:46 -0400

    card change 2
    card change 2

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ card8 = [8, 'Retal Rewards', 'Business', 'Low/No Annual Fee', 'Capital One Ventu
 card9 = [9, 'Balance Transfer', 'Student', 'High Reward Rate', 'Capital One SavorOne Cash Rewards Credit Card', 630, 689]
 card10 = [10, 'Airline Miles', 'Business', 'User Rated 5/5', 'U.S. Bank Altitude Connect Visa Signature Card', 720, 850]
 
+cards = [card1, card2, card3, card4, card5, card6, card7, card8, card9, card10]
+
 #Home Directory Path
 @app.route("/")
 def home():
@@ -35,9 +37,11 @@ def search():
         common_benefits = request.form.get("benefit")
         occupation = request.form.get("occupation")
         other =  request.form.get("other")
-        if(common_benefits == "Hotel and Travel Points"):
-            return "This is a hotel boss"
+        credit_score = 730
 
+        for card in cards:
+            if (card[2] == common_benefits or card[3] == occupation or card[4] == other) and (credit_score >= card[6] and credit_score <= card[7]):
+                return "PLz complete this if statement"
         return "Your benefits are " + common_benefits + "\n" + "Your occupation is " + occupation + "\n" + "Your occupation is " + other
     return render_template("search.html")
 
